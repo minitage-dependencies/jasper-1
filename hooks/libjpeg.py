969f45f7df43d40eef8bc26d0f98b11926aa7aa7
makina <makina@APEM3.(none)>
2009-11-26 10:29:35 +0100

    win32port

--- hooks/libjpeg.py
+++ hooks/libjpeg.py
@@ -1,5 +1,10 @@
 import os
+import sys
 def pre_configure(options, buildout):
     """Custom pre-make hook for building libjpeg."""
+    if sys.platform.startswith('cygwin'):
+        c = os.getcwd()
+        os.chdir(options['compile-directory'])
+        os.system("autoreconf -ifv")
     os.system("chmod +x %s/configure" % buildout['part']['compile-directory'])
 
